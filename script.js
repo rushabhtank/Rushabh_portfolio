0bcce124b86a041b6fc89efa38dc2f8f852a8f0f
rushabhtank <rishitank08@gmail.com>
2023-07-22 21:56:07 +0530

    update

---
+++ script.js
@@ -0,0 +1,44 @@
+const design_card_butttons = document.querySelectorAll('.design-card');
+const introduction_text = document.querySelectorAll('.introduction-text');
+
+const single_profile_card = document.querySelectorAll('.single-profile-card');
+const work_card = document.querySelectorAll('.work-card');
+
+design_card_butttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        introduction_text.forEach((introduction, introductionIndex) => {
+            if (index === introductionIndex) {
+                introduction.style.display = 'block';
+            } else {
+                introduction.style.display = 'none';
+            }
+        });
+        design_card_butttons.forEach((btn, btnIndex) => {
+            if (index === btnIndex) {
+                btn.classList.add('active');
+            } else {
+                btn.classList.remove('active');
+                
+            }
+        });
+    });
+});
+
+single_profile_card.forEach((btn, index) => {
+    btn.addEventListener('click', ()=> {
+        work_card.forEach((workCard, workCardIndex) => {
+            if (index === workCardIndex) {
+                workCard.style.display = 'block';
+            } else {
+                workCard.style.display = 'none';
+            }
+        });
+        single_profile_card.forEach((cardBtn, cardIndex) => {
+            if (index === cardIndex) {
+                cardBtn.classList.add('profile-card-active');
+            } else {
+                cardBtn.classList.remove('profile-card-active');
+            }
+        });
+    });
+});
