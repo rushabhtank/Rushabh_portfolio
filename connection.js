fa962c3077421b6ac0128f040e1e150aed45fbd2
Rushabh Rajesh Tank <69869654+rushabhtank@users.noreply.github.com>
2023-07-23 13:41:51 +0000

    firebaseTest

--- connection.js
+++ connection.js
@@ -9,25 +9,50 @@ const firebaseConfig = {
     measurementId: "G-CMPN2K6BQX"
   };
 
-firebase.initializeApp(firebaseConfig);
 
-//   reference database
+  firebase.initializeApp(firebaseConfig);
 
-var contactFormDB = firebase.database().ref("form");
-
-document.getElementById("form").addEventListener("submit",submitForm);
-
-function submitForm(e){
+  // Reference messages collection
+  var messagesRef = firebase.database().ref('messages');
+  
+  // Listen for form submit
+  document.getElementById('contactForm').addEventListener('submit', submitForm);
+  
+  // Submit form
+  function submitForm(e){
     e.preventDefault();
-
-    var name = getElementVal('name');
-    var email = getElementVal('email');
-    var num = getElementVal('number');
-
-    console.log(name, email, num);
-
-}
-
-const getElementVal = (id) =>{
+  
+    // Get values
+    var name = getInputVal('name');
+    var email = getInputVal('email');
+    var number = getInputVal('number');
+  
+    // Save message
+    saveMessage(name, email, phone);
+  
+    // Show alert
+    document.querySelector('.alert').style.display = 'block';
+  
+    // Hide alert after 3 seconds
+    setTimeout(function(){
+      document.querySelector('.alert').style.display = 'none';
+    },3000);
+  
+    // Clear form
+    document.getElementById('contactForm').reset();
+  }
+  
+  // Function to get get form values
+  function getInputVal(id){
     return document.getElementById(id).value;
-};
+  }
+  
+  // Save message to firebase
+  function saveMessage(name, email, number){
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+      name: name,
+      email:email,
+      number:number,
+    });
+  }
